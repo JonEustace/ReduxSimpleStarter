3d1824eb54328938e30607fd2336c5dbc419ee60
Jonathan Eustace <jeustace@tangledpixel.ca>
2017-01-15 07:16:30 -0800

    We now setState with the value of the searchbar and then print out that value within a div.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -6,12 +6,20 @@ class SearchBar extends Component {
         super(props);
 
         // Here we record the state of the search term.
+        // We never modify our state property directly unless it's in our constructor function.
+        // We use this.setState to access state elsewhere. this.setState is used by
+        // react so it knows that the state has changed.
         this.state = { term: '' };
     }
 
-    // We render the input of the search bar on change to the console. 
+    // We render the input of the search bar and set it to state.
     render(){
-       return <input onChange={event => console.log(event.target.value)} />;
+       return (
+           <div>
+               <input onChange={event => this.setState({ term: event.target.value })} />
+               Value of the input: {this.state.term}
+           </div>
+    )
     }
 
 }
