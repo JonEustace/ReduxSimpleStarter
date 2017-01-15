b389f14e6f5f1920dac5ef0ff909a25af11f83d3
Jonathan Eustace <jeustace@tangledpixel.ca>
2017-01-15 07:09:09 -0800

    Added a constructor that calls it's super. We also define the state property in the constructor. The state property has an object with one property: term, sort for search term.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+
+    constructor(props){
+        super(props);
+
+        // Here we record the state of the search term.
+        this.state = { term: '' };
+    }
+
+    // We render the input of the search bar on change to the console. 
     render(){
        return <input onChange={event => console.log(event.target.value)} />;
     }
