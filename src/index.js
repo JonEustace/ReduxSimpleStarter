38a91253c68b4419b77ed2584a984d6f00f1554c
Jonathan Eustace <jeustace@tangledpixel.ca>
2017-01-15 07:43:28 -0800

    Import youtube-api-search and perform a test call to the API using our key.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import API_KEY from '../keys';
+import YTSearch from 'youtube-api-search';
 
+// We used downwards data flow - The most parent component should be in charge of fetching external data.
 import SearchBar from './components/search_bar';
 
+// test API call.
+YTSearch({key: API_KEY, term: 'surfboards'}, data => console.log(data));
 
 // Create a new component. This component should produce some HTML.
 // Functional components are used when we are taking in some information and spitting out some
