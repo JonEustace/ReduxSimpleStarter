2e4f3e7f7b50fd209c4634aedaf2d9ff7bb6a7b7
Jonathan Eustace <jeustace@tangledpixel.ca>
2017-01-15 15:57:07 -0800

    We build out the video_list_item component. We take the imageurl and the title of the video and wrap it in HTML with some bootstrap classes.

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
-    return <li>{props.video.snippet.description}</li>
+// the {video} as the function's signature is the same as saying
+// const video = props.video;
+const VideoListItem = ({video}) => {
+    const imageUrl = video.snippet.thumbnails.default.url;
+
+    return (
+        <li className="list-group-item">
+            <div className="video-list media">
+                <div className="media-left">
+                    <img className="media-object" src={imageUrl} />
+                </div>
+            </div>
+
+            <div className="media-body">
+                <div className="media-heading">{video.snippet.title}</div>
+            </div>
+
+        </li>
+    )
 };
 
 export default VideoListItem;
